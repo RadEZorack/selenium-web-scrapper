81a4ed354f9b7ed4d51ee91f5b6fb873e9f8ef10
Travis Miller <radezorack@gmail.com>
2024-09-02 03:26:15 -0400

    Updated Tests and moved test

--- app/tests/test_selenium.py
+++ app/tests/test_selenium.py
@@ -1,10 +1,11 @@
 import pytest
-from main import setup_driver, search_and_extract
 
-def test_search_and_extract():
+from ..main import setup_driver, scrape_hacker_news
+
+def test_scrape_hacker_news():
     driver = setup_driver()
     try:
-        results = search_and_extract(driver)
+        results = scrape_hacker_news(driver)
         assert len(results) > 0, "No results found!"
         assert all(len(item) == 2 for item in results), "Results do not have the correct format!"
     finally:
